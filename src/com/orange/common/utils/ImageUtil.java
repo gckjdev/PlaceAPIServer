7889a60d422e233cd3c0cf19cdfa2415cd138fec
gamyhuang <hguangm2009@gmail.com>
2011-05-26 17:52:13 +0800

    Add ImageUtil class to get the thumb image.

---
+++ src/com/orange/common/utils/ImageUtil.java
@@ -0,0 +1,70 @@
+package com.orange.common.utils;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+
+public class ImageUtil {
+
+	private static String SmallImageSuffix = "_s";
+
+	public static String getSmallImageName(File imageFile) {
+		if (imageFile == null)
+			return null;
+		String imageName = imageFile.getName();
+		int index = imageName.lastIndexOf(".");
+		if (index < 0)
+			return null;
+		String smallImageName = imageName.substring(0, index)
+				+ SmallImageSuffix + imageName.substring(index);
+		return smallImageName;
+	}
+
+	public static File setImageSize(File imageFile, double scale) {
+		Image src = null;
+		File outFile = null;
+		try {
+			src = ImageIO.read(imageFile);
+			int width = src.getWidth(null);
+			int height = src.getHeight(null);
+			width = (int) Math.round(width * scale);
+			height = (int) Math.round(height * scale);
+			String name = getSmallImageName(imageFile);
+			outFile = setImageSize(imageFile, width, height, name);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		return outFile;
+	}
+
+	public static File setImageSize(File imageFile, int width, int height,
+			String outImageName) {
+		Image src = null;
+		File outFile = null;
+		try {
+			src = ImageIO.read(imageFile);
+
+			BufferedImage tag = new BufferedImage(width, height,
+					BufferedImage.TYPE_INT_RGB);
+			tag.getGraphics().drawImage(src, 0, 0, width, height, null); // 绘制缩小后的图
+			outFile = new File(outImageName);
+			FileOutputStream newimage = new FileOutputStream(outFile);
+			JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(newimage);
+			encoder.encode(tag); // 近JPEG编码
+			newimage.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return outFile;
+	}
+}
