b55080263ed4ac0683f0c5cdc5c46d325dd68dbb
gamyhuang <hguangm2009@gmail.com>
2011-06-05 18:45:31 +0800

    fix upload file method.

--- src/com/orange/place/upload/ImageUploadManager.java
+++ src/com/orange/place/upload/ImageUploadManager.java
@@ -9,36 +9,50 @@ import com.orange.place.constant.ErrorCode;
 
 public class ImageUploadManager extends AbstractUploadManager {
 
+	private String thumbImage = null;
+
 	public String uploadImageWithCompression(HttpServletRequest request) {
-		String filePath = uploadFile(request);
-		if (filePath == null){
+		String httpPath = uploadFile(request);
+		if (httpPath == null) {
 			resultCode = ErrorCode.ERROR_UPLOAD_FILE;
 			return null;
 		}
-		
+
 		String localPath = getLocalFilePath();
 		File imageFile = new File(localPath);
-		if (!imageFile.exists()){
+		if (!imageFile.exists()) {
 			resultCode = ErrorCode.ERROR_CREATE_THUNMB_FILEPATH;
-			log.info("Error: <uploadImageWithCompression> Could not find the upload image! image file path="+localPath);			
+			log
+					.info("Error: <uploadImageWithCompression> Could not find the upload image! image file path="
+							+ localPath);
 			return null;
 		}
 		File smallImage = ImageUtil.setImageSize(imageFile, 100);
-		if (smallImage == null || !smallImage.exists()){
+		if (smallImage == null || !smallImage.exists()) {
 			resultCode = ErrorCode.ERROR_CREATE_THUNMB_FILE;
-			log.info("Error: <uploadImageWithCompression> fail to create thumnail image, local path="+localPath);
+			log
+					.info("Error: <uploadImageWithCompression> fail to create thumnail image, local path="
+							+ localPath);
 			return null;
 		}
 
-		log.info("<uploadImageWithCompression> create thumbnail image="+smallImage.getAbsolutePath());
-
-		return getFilePath();
+		setThumbImage(ImageUtil.getSmallImagePath(httpPath));
+		log.info("<uploadImageWithCompression> create thumbnail image="
+				+ getThumbImage());
+		return httpPath;
 	}
 
 	public String uploadImage(HttpServletRequest request) {
 		return uploadFile(request);
 	}
-	
+
+	public String getThumbImage() {
+		return thumbImage;
+	}
+
+	public void setThumbImage(String thumbImage) {
+		this.thumbImage = thumbImage;
+	}
 
 	@Override
 	public int getResultCode() {
