905186c6edd2641bbfd8c315085155d91972fdb7
gamyhuang <hguangm2009@gmail.com>
2011-07-02 17:41:18 +0800

    update for action on post

--- src/com/orange/place/constant/DBConstants.java
+++ src/com/orange/place/constant/DBConstants.java
@@ -3,8 +3,8 @@ package com.orange.place.constant;
 public class DBConstants {
 
 
-	//public static final String SERVER = "localhost:9160";
-	public static final String SERVER = "192.168.1.101:9160";
+	public static final String SERVER = "localhost:9160";
+	//public static final String SERVER = "192.168.1.101:9160";
 	public static final String CLUSTERNAME = "Test Cluster";
 
 	public static final String KEYSPACE = "PlaceKS";
@@ -31,6 +31,9 @@ public class DBConstants {
 	public static final String INDEX_POST_LOCATION = "idx_post_location";
 	public static final String INDEX_APPID_POST = "idx_app_post";
 	
+	// column family for counter
+	public static final String COUNTER_POST = "counter_post";
+	
 	public static final String KEY_LOGINID = "loginId";
 	public static final String KEY_DEVICEID = "deviceId";
 
@@ -52,7 +55,7 @@ public class DBConstants {
 	public static final String F_CREATE_SOURCE_ID = "createSourceId";
 	public static final String F_MODIFY_DATE = "modifyDate";
 	public static final String F_MODIFY_SOURCE_ID = "modifySourceId";
-	public static final String F_LIKE_COUNT = "likeCount";
+	//public static final String F_LIKE_COUNT = "likeCount";
 		
 	public static final String F_EMAIL = "user_email";
 	public static final String F_MOBILE = "mobile";
@@ -108,7 +111,7 @@ public class DBConstants {
 	public static final String F_REPLY_POSTID = "replyPostId";
 
 	// to be refactored
-	public static final String F_ACTION_LIKE_COUNT = "$action$like";		// for post action LIKE, store counter
+	//public static final String F_COUNTER_TYPE_POST_LIKE = "$action$like";		// for post action LIKE, store counter
 	
 	//DB App fields
 //	public static final String F_APPID = "appid";
