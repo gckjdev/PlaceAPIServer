e0ffefe3b4cc0c5c7968d5fe58be3bea5425965e
Benson Peng <penglingzhe@gmail.com>
2011-07-04 11:42:01 +0800

    add PostType.ACTION and statistic log in ActionOnPostService

--- src/com/orange/place/api/service/ActionOnPostService.java
+++ src/com/orange/place/api/service/ActionOnPostService.java
@@ -4,6 +4,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import net.sf.json.JSONObject;
 
+import com.orange.place.analysis.domain.AnalysisLogContent;
+import com.orange.place.analysis.domain.PostType;
+import com.orange.place.analysis.log.AnalysisLogUtil;
 import com.orange.place.constant.ErrorCode;
 import com.orange.place.constant.ServiceConstant;
 import com.orange.place.manager.PostManager;
@@ -13,19 +16,30 @@ public class ActionOnPostService extends CommonService {
 	String postId;
 	String postActionType;
 	String appId;
+	String placeId;
+	String latitude;
+	String longitude;
 
 	@Override
 	public String toString() {
-		return "ActionOnPostService [appId=" + appId + ", postActionType="
-				+ postActionType + ", postId=" + postId + ", userId=" + userId
-				+ "]";
-	}
+		return "ActionOnPostService [appId=" + appId + ", latitude=" + latitude
+				+ ", longitude=" + longitude + ", placeId=" + placeId
+				+ ", postActionType=" + postActionType + ", postId=" + postId
+				+ ", userId=" + userId + "]";
+	}		
 
 	@Override
 	public void handleData() {
-		// TODO Auto-generated method stub
+		
+		// write to post action counter column family
 		long count = PostManager.actionOnPost(cassandraClient, postId, userId,
 				postActionType);
+		
+		//  log for analysis
+		AnalysisLogContent content = new AnalysisLogContent().setLatitude(latitude).setLongitude(longitude).setPlaceId(placeId)
+				.setPostId(postId).setPostType(PostType.ACTION).setUserId(userId);
+		AnalysisLogUtil.log(content);
+		
 		JSONObject obj = new JSONObject();
 		obj.put(postActionType, Long.toString(count));
 		resultData = obj;
@@ -50,6 +64,9 @@ public class ActionOnPostService extends CommonService {
 		postActionType = request
 				.getParameter(ServiceConstant.PARA_POST_ACTION_TYPE);
 		appId = request.getParameter(ServiceConstant.PARA_APPID);
+		latitude = request.getParameter(ServiceConstant.PARA_LATITUDE);
+		longitude = request.getParameter(ServiceConstant.PARA_LONGTITUDE);
+		placeId = request.getParameter(ServiceConstant.PARA_PLACEID);		
 		
 		if (!check(postId, ErrorCode.ERROR_PARAMETER_POSTID_EMPTY,
 				ErrorCode.ERROR_PARAMETER_POSTID_NULL))
